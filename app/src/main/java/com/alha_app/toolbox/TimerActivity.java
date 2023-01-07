ba15a0c04cf725ff1a762f4e49df4883707c1926
Akito <al6.vha12@gmail.com>
2023-01-07 18:42:34 +0900

    タイマーのレイアウトの作成

--- app/src/main/java/com/alha_app/toolbox/TimerActivity.java
+++ app/src/main/java/com/alha_app/toolbox/TimerActivity.java
@@ -2,6 +2,7 @@ package com.alha_app.toolbox;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,41 @@ public class TimerActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
+
+        NumberPicker numberPicker1 = findViewById(R.id.numberPiker1);
+        NumberPicker numberPicker2 = findViewById(R.id.numberPiker2);
+        NumberPicker numberPicker3 = findViewById(R.id.numberPiker3);
+        String hours[] = new String[100];
+        String minutes[] = new String[60];
+        String seconds[] = new String[60];
+
+        for(int i = 0; i < hours.length; i++){
+            if(i < minutes.length){
+                if(i < 10){
+                    minutes[i] = "0" + String.valueOf(i);
+                    seconds[i] = "0" + String.valueOf(i);
+                }else{
+                    minutes[i] = String.valueOf(i);
+                    seconds[i] = String.valueOf(i);
+                }
+            }
+
+            if(i < 10){
+                hours[i] = "0" + String.valueOf(i);
+            }else{
+                hours[i] = String.valueOf(i);
+            }
+        }
+        numberPicker1.setMaxValue(99);
+        numberPicker1.setMinValue(0);
+        numberPicker2.setMaxValue(59);
+        numberPicker2.setMinValue(0);
+        numberPicker3.setMaxValue(59);
+        numberPicker3.setMinValue(0);
+
+        numberPicker1.setDisplayedValues(hours);
+        numberPicker2.setDisplayedValues(minutes);
+        numberPicker3.setDisplayedValues(seconds);
     }
 
     @Override
