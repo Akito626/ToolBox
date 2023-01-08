df8b8bfd123ab2ad7e66dc2a23c92f3a95908494
Akito <al6.vha12@gmail.com>
2023-01-08 13:14:46 +0900

    タイマーが開始すると、レイアウトを切り替えてタイマーが動作するようにしました。音楽ファイルを切り替えられるようにしました。

--- app/src/main/java/com/alha_app/toolbox/TimerActivity.java
+++ app/src/main/java/com/alha_app/toolbox/TimerActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
@@ -31,7 +32,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAmount;
+import java.util.Calendar;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerActivity extends AppCompatActivity {
     private final String mFileName = "Timer.txt";
@@ -41,13 +50,19 @@ public class TimerActivity extends AppCompatActivity {
     NumberPicker numberPicker1;
     NumberPicker numberPicker2;
     NumberPicker numberPicker3;
+    private Timer timer;
     TextView musicText;
     private MediaPlayer mp;
+    // 保存用
     private String hour;
     private String minute;
     private String second;
     private String music;
     private String path;
+    // 現在値
+    private int nphour;
+    private int npminute;
+    private int npsecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,67 +73,15 @@ public class TimerActivity extends AppCompatActivity {
         Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
 
         // デフォルトの設定
-        mp = MediaPlayer.create(this, R.raw.defaultmusic);
-        mp.setLooping(true);
         music = "defauntmusic";
         path = null;
-        hour = "0";
-        minute = "0";
-        second = "0";
-
-        numberPicker1 = findViewById(R.id.numberPiker1);
-        numberPicker2 = findViewById(R.id.numberPiker2);
-        numberPicker3 = findViewById(R.id.numberPiker3);
-        String hours[] = new String[100];
-        String minutes[] = new String[60];
-        String seconds[] = new String[60];
+        hour = minute = second = "0";
+        nphour = npminute = npsecond = 0;
 
         musicText = findViewById(R.id.musictext);
+
         Button startButton = findViewById(R.id.startbutton);
         Button resetButton = findViewById(R.id.resetbutton);
-
-        // NumberPicker用の配列を作成
-        for(int i = 0; i < hours.length; i++){
-            if(i < minutes.length){
-                if(i < 10){
-                    minutes[i] = "0" + String.valueOf(i);
-                    seconds[i] = "0" + String.valueOf(i);
-                }else{
-                    minutes[i] = String.valueOf(i);
-                    seconds[i] = String.valueOf(i);
-                }
-            }
-
-            if(i < 10){
-                hours[i] = "0" + String.valueOf(i);
-            }else{
-                hours[i] = String.valueOf(i);
-            }
-        }
-
-        // NumberPickerが変更されると値を取得
-        NumberPicker.OnValueChangeListener pickerListener = new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                hour = String.valueOf(numberPicker1.getValue());
-                minute = String.valueOf(numberPicker2.getValue());
-                second = String.valueOf(numberPicker3.getValue());
-            }
-        };
-
-        // NumberPickerの設定
-        numberPicker1.setMaxValue(99);
-        numberPicker1.setMinValue(0);
-        numberPicker2.setMaxValue(59);
-        numberPicker2.setMinValue(0);
-        numberPicker3.setMaxValue(59);
-        numberPicker3.setMinValue(0);
-        numberPicker1.setDisplayedValues(hours);
-        numberPicker2.setDisplayedValues(minutes);
-        numberPicker3.setDisplayedValues(seconds);
-        numberPicker1.setOnValueChangedListener(pickerListener);
-        numberPicker2.setOnValueChangedListener(pickerListener);
-        numberPicker3.setOnValueChangedListener(pickerListener);
 
         View.OnTouchListener timertouchListener = new View.OnTouchListener() {
             @Override
@@ -149,12 +112,63 @@ public class TimerActivity extends AppCompatActivity {
                 String str = button.getText().toString();
 
                 if(str.equals("スタート")){
-                    button.setText("停止");
+                    if(nphour == 0 && npminute == 0 && npsecond == 0) {
+                        Toast.makeText(TimerActivity.this, "値をセットしてください", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // レイアウトの切り替え
+                        LinearLayout layout = findViewById(R.id.stoplayout);
+                        layout.removeAllViews();
+                        getLayoutInflater().inflate(R.layout.activity_timerrun, layout);
+                        TextView timeText = findViewById(R.id.timetext);
+                        TextView stoptimeText = findViewById(R.id.stoptimetext);
+                        // 時間の取得
+                        LocalTime localTime = LocalTime.now(ZoneId.of("Japan"));
+                        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("HH:mm:ss");
+                        localTime = localTime.plusHours(nphour);
+                        localTime = localTime.plusMinutes(npminute);
+                        localTime = localTime.plusSeconds(npsecond);
+                        // タイマーが止まる時間を表示
+                        stoptimeText.setText(localTime.format(sdf));
+                        // タイマーの生成
+                        timer = new Timer();
+                        TimerTask timerTask = new TimerTask() {
+                            @Override
+                            public void run() {
+                                npsecond--;
+                                if (nphour == 0 && npminute == 0 && npsecond == 0) {
+                                    timeText.setText(String.format("%02d:%02d:%02d", nphour, npminute, npsecond));
+                                    timer.cancel();
+                                    mp.start();
+                                    return;
+                                }
+                                if (npsecond == -1) {
+                                    npsecond = 59;
+                                    npminute--;
+                                }
+                                if (npminute == -1) {
+                                    npminute = 59;
+                                    nphour--;
+                                }
+                                timeText.setText(String.format("%02d:%02d:%02d", nphour, npminute, npsecond));
+                            }
+                        };
+                        timer.schedule(timerTask, 0, 1000);
+                        button.setText("停止");
+                    }
                 }else if(str.equals("リセット")){
-
+                    numberPicker1.setValue(Integer.parseInt(hour));
+                    numberPicker2.setValue(Integer.parseInt(minute));
+                    numberPicker3.setValue(Integer.parseInt(second));
                 }else if(str.equals("停止")){
-                    mp.stop();
-                    mp.reset();
+                    LinearLayout layout = findViewById(R.id.runlayout);
+                    layout.removeAllViews();
+                    getLayoutInflater().inflate(R.layout.activity_timerstop, layout);
+                    prepareLayout();
+                    timer.cancel();
+                    if(mp.isPlaying()) {
+                        mp.stop();
+                        mp.reset();
+                    }
                     button.setText("スタート");
                 }
             }
@@ -164,6 +178,7 @@ public class TimerActivity extends AppCompatActivity {
         startButton.setOnClickListener(btnListener);
         resetButton.setOnClickListener(btnListener);
 
+        prepareLayout();
         loadFile();
     }
 
@@ -177,6 +192,21 @@ public class TimerActivity extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         loadFile();
+
+        if(path != null){
+            try {
+                mp = new MediaPlayer();
+                mp.setDataSource(path);
+                mp.prepare();
+            }catch (Exception e){
+                mp = MediaPlayer.create(this, R.raw.defaultmusic);
+            }
+        } else{
+            mp = MediaPlayer.create(this, R.raw.defaultmusic);
+        }
+
+        mp.setLooping(true);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
 
     @Override
@@ -197,8 +227,64 @@ public class TimerActivity extends AppCompatActivity {
         return result;
     }
 
-    public void audioPlay(){
+    public void prepareLayout(){
+        numberPicker1 = findViewById(R.id.numberPiker1);
+        numberPicker2 = findViewById(R.id.numberPiker2);
+        numberPicker3 = findViewById(R.id.numberPiker3);
 
+        String hours[] = new String[100];
+        String minutes[] = new String[60];
+        String seconds[] = new String[60];
+
+        // NumberPicker用の配列を作成
+        for(int i = 0; i < hours.length; i++){
+            if(i < minutes.length){
+                if(i < 10){
+                    minutes[i] = "0" + String.valueOf(i);
+                    seconds[i] = "0" + String.valueOf(i);
+                }else{
+                    minutes[i] = String.valueOf(i);
+                    seconds[i] = String.valueOf(i);
+                }
+            }
+
+            if(i < 10){
+                hours[i] = "0" + String.valueOf(i);
+            }else{
+                hours[i] = String.valueOf(i);
+            }
+        }
+
+        // NumberPickerが変更されると値を取得
+        NumberPicker.OnValueChangeListener pickerListener = new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                nphour = numberPicker1.getValue();
+                npminute = numberPicker2.getValue();
+                npsecond = numberPicker3.getValue();
+                hour = String.valueOf(nphour);
+                minute = String.valueOf(npminute);
+                second = String.valueOf(npsecond);
+            }
+        };
+
+        // NumberPickerの設定
+        numberPicker1.setMaxValue(99);
+        numberPicker1.setMinValue(0);
+        numberPicker2.setMaxValue(59);
+        numberPicker2.setMinValue(0);
+        numberPicker3.setMaxValue(59);
+        numberPicker3.setMinValue(0);
+        numberPicker1.setDisplayedValues(hours);
+        numberPicker2.setDisplayedValues(minutes);
+        numberPicker3.setDisplayedValues(seconds);
+        numberPicker1.setOnValueChangedListener(pickerListener);
+        numberPicker2.setOnValueChangedListener(pickerListener);
+        numberPicker3.setOnValueChangedListener(pickerListener);
+
+        numberPicker1.setValue(nphour);
+        numberPicker2.setValue(npminute);
+        numberPicker3.setValue(npsecond);
     }
 
     public void loadFile(){
@@ -236,12 +322,19 @@ public class TimerActivity extends AppCompatActivity {
             } catch (Exception e) {
                 Toast.makeText(this, "File read error!", Toast.LENGTH_LONG).show();
             }
-        }
 
-        numberPicker1.setValue(Integer.parseInt(hour));
-        numberPicker2.setValue(Integer.parseInt(minute));
-        numberPicker3.setValue(Integer.parseInt(second));
-        musicText.setText(music);
+            if (hour.matches("[\\d]")) {
+                nphour = Integer.parseInt(hour);
+                npminute = Integer.parseInt(minute);
+                npsecond = Integer.parseInt(second);
+                numberPicker1.setValue(nphour);
+                numberPicker2.setValue(npminute);
+                numberPicker3.setValue(npsecond);
+                musicText.setText(music);
+            } else {
+                files[0].delete();
+            }
+        }
     }
 
     public void saveFile(){
