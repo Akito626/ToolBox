9e1cb16f5da071de3ea18dae2ea8a68cb663d4e8
Akito <al6.vha12@gmail.com>
2023-06-30 00:04:44 +0900

    取得したデータをmapに保存するように変更

--- app/src/main/java/com/alha_app/toolbox/WeatherActivity.java
+++ app/src/main/java/com/alha_app/toolbox/WeatherActivity.java
@@ -1,25 +1,29 @@
 package com.alha_app.toolbox;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.JsonReader;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class WeatherActivity extends AppCompatActivity {
-    ExecutorService es = Executors.newSingleThreadExecutor();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +32,18 @@ public class WeatherActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
 
+        final Handler handler = new Handler();
+
         // 別スレッドで天気を取得
-        es.execute(() -> getCurrentWeather());
+        new Thread(() -> {
+            String json = getCurrentWeather();
+            handler.post(() -> {
+                TextView textView = findViewById(R.id.weather_text);
+                textView.setText(json);
+                LinearLayout linearLayout = findViewById(R.id.weather_Load);
+                linearLayout.setVisibility(View.GONE);
+            });
+        }).start();
     }
 
     @Override
@@ -50,11 +64,14 @@ public class WeatherActivity extends AppCompatActivity {
         return result;
     }
 
-    public void getCurrentWeather(){
+    public String getCurrentWeather(){
         String key = BuildConfig.KEY;
         String urlString = "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=" + key + "&lang=ja&units=metric";
-        String result = "";
+        String json = "";
         StringBuilder sb = new StringBuilder();
+
+        Map<String, Object> map = null;
+        ObjectMapper mapper = new ObjectMapper();
 
         try {
             URL url = new URL(urlString);
@@ -65,13 +82,16 @@ public class WeatherActivity extends AppCompatActivity {
             while((tmp = br.readLine()) != null){
                 sb.append(tmp);
             }
-            result = sb.toString();
+            json = sb.toString();
+
+            map = mapper.readValue(json, new TypeReference<Map<String, Object>>(){});
 
             br.close();
             con.disconnect();
         } catch (Exception e){
             e.printStackTrace();
         }
-        System.out.println(result);
+
+        return json;
     }
 }
