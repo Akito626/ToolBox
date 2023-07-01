fd08651169f5427fc8747272887165f646e20679
Akito <al6.vha12@gmail.com>
2023-07-01 22:13:03 +0900

    WeatherActivityの項目をタブで切り替えられるようにしました

---
+++ app/src/main/java/com/alha_app/toolbox/TodayWeatherFragment.java
@@ -0,0 +1,31 @@
+package com.alha_app.toolbox;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TodayWeatherFragment extends Fragment {
+
+    public static TodayWeatherFragment newInstance(){
+        TodayWeatherFragment fragment = new TodayWeatherFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_todayweather,
+                container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
