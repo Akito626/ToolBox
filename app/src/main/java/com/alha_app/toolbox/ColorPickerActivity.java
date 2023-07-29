18064dd5b095e9bd69e2a8b95a66f6472090e829
Akito <al6.vha12@gmail.com>
2023-07-29 17:52:03 +0900

    カラーピッカーでhsvから色を選択できるように変更

--- app/src/main/java/com/alha_app/toolbox/ColorPickerActivity.java
+++ app/src/main/java/com/alha_app/toolbox/ColorPickerActivity.java
@@ -40,6 +40,7 @@ public class ColorPickerActivity extends AppCompatActivity {
         EditText rgbText = findViewById(R.id.rgb_text);
         TextView hexText = findViewById(R.id.hex_text);
         EditText cmykText = findViewById(R.id.cmyk_text);
+        EditText hsvText = findViewById(R.id.hsv_text);
         View nowColor = findViewById(R.id.now_color);
         ColorPickerView colorPicker = findViewById(R.id.color_picker);
 
@@ -100,22 +101,57 @@ public class ColorPickerActivity extends AppCompatActivity {
                     if(cmykStr.length != 4) return;
                     Pattern pattern = Pattern.compile("^[0-9]+$|-[0-9]+$");
                     if(pattern.matcher(cmykStr[0]).matches()){
-                        if(Integer.parseInt(cmykStr[0]) > 100) return;
+                        if(Integer.parseInt(cmykStr[0]) > 100 || Integer.parseInt(cmykStr[0]) < 0) return;
                         c = Integer.parseInt(cmykStr[0]);
                     }
                     if(pattern.matcher(cmykStr[1]).matches()){
-                        if(Integer.parseInt(cmykStr[1]) > 100) return;
+                        if(Integer.parseInt(cmykStr[1]) > 100 || Integer.parseInt(cmykStr[1]) < 0) return;
                         m = Integer.parseInt(cmykStr[1]);
                     }
                     if(pattern.matcher(cmykStr[2]).matches()){
-                        if(Integer.parseInt(cmykStr[2]) > 100) return;
+                        if(Integer.parseInt(cmykStr[2]) > 100 || Integer.parseInt(cmykStr[2]) < 0) return;
                         y = Integer.parseInt(cmykStr[2]);
                     }
                     if(pattern.matcher(cmykStr[3]).matches()){
-                        if(Integer.parseInt(cmykStr[3]) > 100) return;
+                        if(Integer.parseInt(cmykStr[3]) > 100 || Integer.parseInt(cmykStr[3]) < 0) return;
                         k = Integer.parseInt(cmykStr[3]);
                     }
                     int color = cmykToRGB(c, m, y, k);
+                    colorPicker.setColor(color);
+                    nowColor.setBackgroundColor(color);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        hsvText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(hsvText.hasFocus()){
+                    String[] hsvStr = s.toString().replaceAll(" ", "").replaceAll("%", "").replaceAll("°", "").split(",");
+                    if(hsvStr.length != 3) return;
+                    Pattern pattern = Pattern.compile("^[0-9]+$|-[0-9]+$");
+                    if(pattern.matcher(hsvStr[0]).matches()){
+                        if(Integer.parseInt(hsvStr[0]) > 100 || Integer.parseInt(hsvStr[0]) < 0) return;
+                        r = Integer.parseInt(hsvStr[0]);
+                    }
+                    if(pattern.matcher(hsvStr[1]).matches()){
+                        if(Integer.parseInt(hsvStr[1]) > 100 || Integer.parseInt(hsvStr[1]) < 0) return;
+                        g = Integer.parseInt(hsvStr[1]);
+                    }
+                    if(pattern.matcher(hsvStr[2]).matches()){
+                        if(Integer.parseInt(hsvStr[2]) > 100 || Integer.parseInt(hsvStr[2]) < 0) return;
+                        b = Integer.parseInt(hsvStr[2]);
+                    }
+                    int color = hsvToRGB(r, g, b);
                     colorPicker.setColor(color);
                     nowColor.setBackgroundColor(color);
                 }
@@ -144,6 +180,7 @@ public class ColorPickerActivity extends AppCompatActivity {
             hexText.setText("#" + hr + hg + hb);
 
             rgbToCMYK(r, g, b);
+            rgbToHSV(r, g, b);
         });
     }
 
@@ -171,9 +208,7 @@ public class ColorPickerActivity extends AppCompatActivity {
         double cmykG = (double) g / 255;
         double cmykB = (double) b / 255;
 
-        double max;
-        max = Math.max(cmykR, cmykG);
-        max = Math.max(max, cmykB);
+        double max = max3(cmykR, cmykG, cmykB);
         double k = 1 - max;
         double c = (1 - cmykR - k) / (1 - k) * 100;
         double m = (1 - cmykG - k) / (1 - k) * 100;
@@ -201,5 +236,83 @@ public class ColorPickerActivity extends AppCompatActivity {
         rgbText.setText(String.format("%3d, %3d, %3d", (int) Math.round(r), (int) Math.round(g), (int) Math.round(b)));
 
         return color;
+    }
+
+    // rgbからhsvに変換してhsvを表示
+    private void rgbToHSV(int r, int g, int b){
+        double max = max3(r, g, b);
+        double min = min3(r, g, b);
+
+        double h = 0;
+        if(r == g && g == b){
+            h = 0;
+        } else if(r == max){
+            h = 60 * ((g - b) / (max - min));
+            if(h < 0){
+                h = 360 + h;
+            }
+        } else if(g == max){
+            h = 60 * ((b - r) / (max - min)) + 120;
+        } else if(b == max){
+            h = 60 * ((r - g) / (max - min)) + 240;
+        }
+        double s = (max - min) / max * 100;
+        double v = max / 255 * 100;
+
+        EditText hsvText = findViewById(R.id.hsv_text);
+        hsvText.setText((int) h + "°, " + (int) s + "%, " + (int) v + "%");
+    }
+
+    private int hsvToRGB(double h, double s, double v){
+        double max = v / 100 * 255;
+        double min = max - ((s / 100) * max);
+
+        int r;
+        int g;
+        int b;
+
+        if(h <= 60){
+            r = (int) Math.round(max);
+            g = (int) Math.round(((h / 100 * 255 / 60) * (max - min) + min));
+            b = (int) Math.round(min);
+        } else if(h <= 120){
+            r = (int) Math.round((((120 - h / 100 * 255) * (max - min) + min)));
+            g = (int) Math.round(max);
+            b = (int) Math.round(min);
+        } else if(h <= 180){
+            r = (int) Math.round(min);
+            g = (int) Math.round(max);
+            b = (int) Math.round((((h / 100 * 255 - 120) / 60 * (max - min) + min)));
+        } else if(h <= 240){
+            r = (int) Math.round(min);
+            g = (int) Math.round(((240 - h / 100 * 255) / 60 * (max - min) + min));
+            b = (int) Math.round(max);
+        } else if(h <= 300){
+            r = (int) Math.round(((h / 100 * 255 - 240) / 60 * (max - min) + min));
+            g = (int) Math.round(min);
+            b = (int) Math.round(max);
+        } else {
+            r = (int) Math.round(max);
+            g = (int) Math.round(min);
+            b = (int) Math.round(((360 - h / 100 * 255) / 60 * (max - min) + min));
+        }
+
+        EditText rgbText = findViewById(R.id.rgb_text);
+        rgbText.setText(String.format("%3d, %3d, %3d", r, g, b));
+
+        int color = Color.rgb(r, g, b);
+        return color;
+    }
+
+    private double max3(double num1, double num2, double num3){
+        double max = Math.max(num1, num2);
+        max = Math.max(max, num3);
+        return max;
+    }
+
+    private double min3(double num1, double num2, double num3){
+        double min = Math.min(num1, num2);
+        min = Math.min(min, num3);
+        return min;
     }
 }
